8d4e061194d64db8dd6acbdb3743663d5adb35db
Mohamed Yehiia <myehiah@gmail.com>
2025-12-03 20:20:48 +0400

    feat: adds spacing for cards in grid

--- Assets/_Project/Scripts/Systems/Grid.cs
+++ Assets/_Project/Scripts/Systems/Grid.cs
@@ -1,13 +1,21 @@
+using UnityEngine;
+
 [System.Serializable]
 public struct Grid
 {
     public int rows;
     public int cols;
 
-    public Grid(int rows, int cols)
+    [Header("Spacing")]
+    public float horizontalSpacing;
+    public float verticalSpacing;
+
+    public Grid(int rows, int cols, float horizontalSpacing = 0, float verticalSpacing = 0)
     {
         this.rows = rows;
         this.cols = cols;
+        this.horizontalSpacing = horizontalSpacing;
+        this.verticalSpacing = verticalSpacing;
     }
 
     public readonly int TotalCards => rows * cols;
--- Assets/_Project/Scripts/Systems/GridManager.cs
+++ Assets/_Project/Scripts/Systems/GridManager.cs
@@ -7,33 +7,43 @@ public class GridManager : MonoBehaviour
 
     public void CalculateGrid(Grid grid, out Vector2 cardSize, out Vector2[,] positions)
     {
-        // screen dimensions
+        // screen and grid dimensions
         float width = gameBoard.rect.width;
         float height = gameBoard.rect.height;
 
         int rows = grid.rows;
         int cols = grid.cols;
 
+        float horizontalSpacing = grid.horizontalSpacing;
+        float verticalSpacing = grid.verticalSpacing;
+
         // find limiting dimension
-        // Todo: add spacing between cards
         float cellWidth = width / cols;
         float cellHeight = height / rows;
 
-        float size = Mathf.Min(cellWidth, cellHeight);
+        float sizeX = cellWidth - horizontalSpacing;
+        float sizeY = cellHeight - verticalSpacing;
+        float size = Mathf.Min(sizeX, sizeY);
+
         cardSize = new Vector2(size, size);
 
+        // position and center grid
         positions = new Vector2[rows, cols];
 
-        // center grid
-        float offsetX = (width - (cols * size)) / 2f;
-        float offsetY = (height - (rows * size)) / 2f;
+        float cardsWidth = cols * size;
+        float gapsWidth = (cols - 1) * horizontalSpacing;
+        float cardsHeight = rows * size;
+        float gapsHeight = (rows - 1) * verticalSpacing;
+
+        float offsetX = (width - (cardsWidth + gapsWidth)) / 2f;
+        float offsetY = (height - (cardsHeight + gapsHeight)) / 2f;
 
         for (int r = 0; r < rows; r++)
         {
             for (int c = 0; c < cols; c++)
             {
-                float x = offsetX + (c * size) + size / 2f;
-                float y = -(offsetY + (r * size) + size / 2f);
+                float x = offsetX + (c * (size + horizontalSpacing)) + size / 2f;
+                float y = -(offsetY + (r * (size + verticalSpacing)) + size / 2f);
 
                 positions[r, c] = new Vector2(x, y);
             }
